18eb279be454ed019fe5f045e704d9514edb072d
Mohammad Fallah <mohammadafall1h@ufl.edu>
2020-09-01 23:42:29 -0400

    OOP changes

--- src/com/company/CarLoan.java
+++ src/com/company/CarLoan.java
@@ -1,13 +1,32 @@
 package com.company;
 
 public class CarLoan {
-    double carLoan = 24007.56;
-    int loanLength = 5;
-    double interestRate = 6.0833;
-    int downPayment = 2000;
-    double remainingBalance = carLoan - downPayment;
-    int months = loanLength*12;
-    double monthlyBalance = remainingBalance/months;
-    double interest= monthlyBalance*interestRate/100;
-    double monthlyPayment =monthlyBalance + interest;
+    double carLoan;
+    int loanLength;
+    double interestRate;
+    int downPayment;
+
+    public CarLoan (double carLoan, int loanLength, double interestRate, int downPayment){
+        this.carLoan = carLoan;
+        this.loanLength = loanLength;
+        this.interestRate = interestRate;
+        this.downPayment = downPayment;
+    }
+    public double calculatePayment(){
+        double remainingBalance = carLoan - downPayment;
+        int months = loanLength*12;
+        double monthlyBalance = remainingBalance/months;
+        double interest= monthlyBalance*interestRate/100;
+        double monthlyPayment =monthlyBalance + interest;
+        if (loanLength <= 0 || interestRate <= 0) {
+            System.out.println("Error! You must take out a valid car loan.");
+            return 0;
+        } else if (downPayment >= carLoan){
+            System.out.println("The car can be paid in full");
+            return 0;
+
+        }else{
+            return monthlyPayment;
+        }
+    }
 }
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,19 +3,8 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        CarLoan = new CarLoan(){
-
-        };
-        if (loanLength <= 0 || interestRate <= 0) {
-            System.out.println("Error! You must take out a valid car loan.");
-        } else if (downPayment >= carLoan){
-            System.out.println("The car can be paid in full");
-
-        }else{
-        System.out.println(monthlyPayment);
-
-
-        }
+        CarLoan newLoan = new CarLoan(25000,48,5.5,3000);
+        System.out.println(newLoan.calculatePayment());
     }
 }
 
